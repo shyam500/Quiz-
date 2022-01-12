b6e56392493974633562062eca999448b52f5a3d
shyam500 <shyamppk2000@gmail.com>
2022-01-12 22:52:16 +0530

    script completed

--- scripts/main.js
+++ scripts/main.js
@@ -47,39 +47,42 @@ const inp_parent = document.querySelector('#inps');
 let current_quest = 0;
 let points = 0;
 
-uiFunc(quest, questions[current_quest].q);
-uiFunc(aLabel, questions[current_quest].a);
-uiFunc(bLabel, questions[current_quest].b);
-uiFunc(cLabel, questions[current_quest].c);
-
-console.log(questions.length);
-
-function uiFunc(element, text) {
-    element.textContent = text;
-}
-
 inp_parent.addEventListener('click', e => {
     if (e.target.type === 'radio') {
         quizFunc(e.target.id)
     };
 });
 
+// setting questions and answers
+uiFunc(quest, questions[current_quest].q);
+uiFunc(aLabel, questions[current_quest].a);
+uiFunc(bLabel, questions[current_quest].b);
+uiFunc(cLabel, questions[current_quest].c);
 
+// decides what to do when a btn is pressed
 function quizFunc(val) {
-    if (val === questions[current_quest].correct) {
-        points += 1;
-    }
-    if (current_quest >= questions.length + 1){
-        alert(`Your point is `)
-        console.log(current_quest);
-        console.log(questions[current_quest]);
+    if (current_quest + 1 === questions.length) {
+        alert(`Quiz finished,You have earned ${points} points`);
+        window.location.reload();
+    } else {
+        if (val === questions[current_quest].correct) {
+            console.log(current_quest);
+            points += 1;
+        }
         current_quest += 1;
-    }
-    // if (current_quest === questions.length + 1) {
-    //     current_quest = 0;
-    // } else {
-    //     current_quest += 1;
-    // };
-    
-    console.log(current_quest);
+        changeQuestionsFunc();
+    };
+};
+
+
+function changeQuestionsFunc() {
+    uiFunc(quest, questions[current_quest].q);
+    uiFunc(aLabel, questions[current_quest].a);
+    uiFunc(bLabel, questions[current_quest].b);
+    uiFunc(cLabel, questions[current_quest].c);
+};
+
+
+function uiFunc(element, text) {
+    element.textContent = text;
 };
